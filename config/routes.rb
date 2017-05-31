8a440496bd7a094200db4808696381f19b420296
Trevor <trevorhoward2@hotmail.com>
2017-05-30 23:04:42 -0600

    Added cool tab switching show/hide actions on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	root to: 'pages#home'	
 
-
 	namespace :admin do 
 		resources :users, only: :show do 
 			post :generate_new_password_email 
