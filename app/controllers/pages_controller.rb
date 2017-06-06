a74b4b3e25fc712af75a6be92c55fe4041f70ca2
Trevor <trevorhoward2@hotmail.com>
2017-06-06 13:41:35 -0600

    Moved products path to homepage and made tab to nav to it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
 
 	def home
 		@order_items = current_order.order_items
+	  @products = Product.all
+	  @order_item = current_order.order_items.new
 	end
 
 	def subscribe
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	root to: 'pages#home'
 	get "subscribe", to: 'pages#subscribe'
-	get "products", to: 'products#index'
 	resources :order_items, only: [:create, :destroy]
 
 	resource :cart, only: [:show]
