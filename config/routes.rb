86248584961f0a1c9c41d2b9abde325324ed50de
Trevor <trevorhoward2@hotmail.com>
2017-06-13 13:11:32 -0600

    Removed secondary nav bar, moved cart button, simplified homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	root to: 'pages#home'
+	get 'about', to: 'pages#about'
 	get "subscribe", to: 'pages#subscribe'
 	resources :order_items, only: [:create, :destroy, :update]
 
