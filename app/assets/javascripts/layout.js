15f96709d83be98336ceae17301ccef67a605712
Trevor <trevorhoward2@hotmail.com>
2017-06-13 12:46:50 -0600

    Removed customize in nav head and homepage

--- app/assets/javascripts/layout.js
+++
@@ -1,9 +0,0 @@
-$(".dropdown-button").dropdown({
-      inDuration: 700,
-      outDuration: 250,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on click
-      alignment: "left", // Aligns dropdown to left or right edge (works with constrain_width)
-      gutter: 0, // Spacing from edge
-      belowOrigin: true,
-    })
